979ff3e029c70e445820a384e510d3897a898124
Eri <89938497+jas-mauve@users.noreply.github.com>
2022-01-07 00:16:43 +0000

    Initial commit

---
+++ Info.js
@@ -0,0 +1,86 @@
+
+// var tamponsBtn=$('.tamponsbtn');
+
+// tamponsbtn.on("click",tamponFacts);
+
+// function showDescription2() {
+//   var x = document.getElementById("myDIV2");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// function tamponFacts(){
+// var container= $('.contain')
+
+// container.append(`<div> <p class="facts">Tampons are a product that absorbs blood during your period. Tampons are designed to be inserted into the vagina with or without an applicator. Tampons are also one-use which means they should only be used 1 time. 
+// Here are some tampon myths below:
+// 1. Tampons can get lost inside you: FALSE. A tampon can go higher in your vagina, and the string might get tucked away, but it’s not lost forever, it’s in there. This is because the cervix is a barrier between your vagina and uterus so essentially,vagina is the dead end.
+
+
+// </p>
+// </div>`);
+   
+// event.preventDefault();
+// }
+
+
+// var mcupBtn=$('.mcupbtn');
+
+// mcupBtn.on("click",menstrualcupFacts);
+
+// function menstrualcupFacts(){
+// var container= $('.contain')
+
+// container.append(`<div> <p class="facts1">Menstrual cups are one of the best sustainabale period products out there. They are small, flexible, bell-shaped cups that are typically made from silicone. You insert into the vagina to collect period blood rather than absorb it (like tampons). Depending on the brand, you can leave them in for up to 12 hours! It's great for swimming and if you don't want to change products frequently during the day!
+// </p>
+// </div>`);
+   
+// event.preventDefault();
+// }
+
+
+// <section class="buttons">
+
+//        </section> 
+
+
+
+function showDescription1() {
+  var x = document.getElementById("myDiv1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showDescription2() {
+  var x = document.getElementById("myDiv2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showDescription3() {
+  var x = document.getElementById("myDiv3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showDescription4() {
+  var x = document.getElementById("myDiv4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
